e38d2c7e2275315be706146e48aafdae62873e7a
Unknown <devyn.brunker@gmail.com>
2018-09-19 16:19:10 -0600

    Tried doing a quiz and i know i failed but im going to retry it

--- Game Test/Assets/Scripts/CharacterMove.cs
+++ Game Test/Assets/Scripts/CharacterMove.cs
@@ -5,38 +5,46 @@ using UnityEngine;
 public class CharacterMove : MonoBehaviour {
 
 	// Player Movement Variables
-		//Public Uppercase, Private Lowercase
 	public int MoveSpeed;
-	public float JumpHeight = 15;
+	public float JumpHeight;
 
-	//Player Ground Variable
-		//Transform Effects Position, Rotation, And Scale
+	// Player grounded variables
 	public Transform GroundCheck;
 	public float GroundCheckRadius;
 	public LayerMask WhatIsGround;
-	private bool grounded;
+	private bool Grounded;
 
 	// Use this for initialization
 	void Start () {
-		print("Hello World!");
+	
 	}
 	
-	void FixedUpdate (){
-		grounded = Physics2d.OverlapCircle(GroundCheck.position, GroundCheckRadius, WhatIsGround);
+
+	void FixedUpdate () {
+		Grounded = Physics2D.OverlapCircle(GroundCheck.position, GroundCheckRadius, WhatIsGround);
 	}
 
 	// Update is called once per frame
 	void Update () {
-		
-		//This Code Makes The Character Jump
-		if(Input.GetKeyDown (KeyCode.Space)&& grounded){
+
+		// This code makes the character jump
+		if(Input.GetKeyDown (KeyCode.Space)&& Grounded){
 			Jump();
 		}
 
-	
+		// This code makes the character move from side to side using the A&D keys
+		if(Input.GetKey (KeyCode.D)){
+			GetComponent<Rigidbody2D>().velocity = new Vector2(MoveSpeed, GetComponent<Rigidbody2D>().velocity.y);
+			
+		}
+		if(Input.GetKey (KeyCode.A)){
+			GetComponent<Rigidbody2D>().velocity = new Vector2(-MoveSpeed, GetComponent<Rigidbody2D>().velocity.y);
+			
+		}
+
 	}
 
 	public void Jump(){
-		GetComponent<Rigidbody2d>().velocity = new Vector2(GetComponent<Rigidbody2d>().velocity.x, JumpHeight);
+		GetComponent<Rigidbody2D>().velocity = new Vector2(GetComponent<Rigidbody2D>().velocity.x, JumpHeight);
 	}
 }
