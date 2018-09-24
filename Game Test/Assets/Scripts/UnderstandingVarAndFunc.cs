93b1f18887db0eada77664fcae55151ee6d21fa5
Unknown <devyn.brunker@gmail.com>
2018-09-24 12:09:15 -0600

    switched float to double
    leanred that what i wanted to use was double and not float. Double gets more exact numbers but takes more memory

--- Game Test/Assets/Scripts/UnderstandingVarAndFunc.cs
+++ Game Test/Assets/Scripts/UnderstandingVarAndFunc.cs
@@ -9,21 +9,17 @@ public class UnderstandingVarAndFunc : MonoBehaviour {
 	access modifier(public/private) type(int/float/bool) name of
 	variable(any name) and or a value (whole numbers) */
 	public int Abby = 5;	//int means whole number (7 8 9)
-	public float Buck;		//float means numbers with a fractional part (5.7 6.3 9.9)
+	public double Buck;		//double means numbers with a fractional part (5.7 6.3 9.9)
 	private bool chad;		//bool means true or false
-
-	Buck = 4.5;
-
-	printf("How old is Buck");
-	scanf("%f", &Buck);
-	printf("Buck is %f\n", Buck);
 
 	// Use this for initialization
 	/*functions go in either void start or void update*/
 	void Start () {
 
-		//doing debug.log allows me to see whats going on in the console in Unity
-		debug.log (Abby);
+	Buck = 4.5;
+
+	print("How old is Buck");
+	print("Buck is %f\n", Buck);
 
 	}
 	
