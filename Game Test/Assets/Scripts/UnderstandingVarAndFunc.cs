98873111ea85dccc03cc0d4fb6845e2a3ec01624
Unknown <devyn.brunker@gmail.com>
2018-09-24 12:01:59 -0600

    messing with floats and prints

--- Game Test/Assets/Scripts/UnderstandingVarAndFunc.cs
+++ Game Test/Assets/Scripts/UnderstandingVarAndFunc.cs
@@ -8,9 +8,15 @@ public class UnderstandingVarAndFunc : MonoBehaviour {
 	/* To make a variable: 
 	access modifier(public/private) type(int/float/bool) name of
 	variable(any name) and or a value (whole numbers) */
-	public int Abby = 5;
-	public float Buck;
-	private bool Chad;
+	public int Abby = 5;	//int means whole number (7 8 9)
+	public float Buck;		//float means numbers with a fractional part (5.7 6.3 9.9)
+	private bool chad;		//bool means true or false
+
+	Buck = 4.5;
+
+	printf("How old is Buck");
+	scanf("%f", &Buck);
+	printf("Buck is %f\n", Buck);
 
 	// Use this for initialization
 	/*functions go in either void start or void update*/
@@ -18,6 +24,7 @@ public class UnderstandingVarAndFunc : MonoBehaviour {
 
 		//doing debug.log allows me to see whats going on in the console in Unity
 		debug.log (Abby);
+
 	}
 	
 	// Update is called once per frame
