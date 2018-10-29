12486abf1627dd3decee5ba88bb17379bdc71f60
Unknown <devyn.brunker@gmail.com>
2018-10-29 16:15:52 -0600

    Messing with character switch script

---
+++ Better Game Test/Assets/Scripts/CharaterSwitch.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharaterSwitch : MonoBehaviour {
+
+	public bool Will;
+	public bool Liz;
+	public bool Lucy;
+
+	// Use this for initialization
+	void Start () {
+		GetComponent<Will>();
+		GetComponent<Liz>();
+		GetComponent<Lucy>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if(Input.GetKeyDown (KeyCode.I)){
+			Will.enable = true;
+			Liz.enable = false;
+			Lucy.enbale = false;
+		}
+		if(Input.GetKeyDown (KeyCode.O)){
+			Will.enable = false;
+			Liz.enable = true;
+			Lucy.enbale = false;
+		}
+		if(Input.GetKeyDown (KeyCode.P)){
+			Will.enable = false;
+			Liz.enable = false;
+			Lucy.enbale = true;
+		}
+	}
+}
