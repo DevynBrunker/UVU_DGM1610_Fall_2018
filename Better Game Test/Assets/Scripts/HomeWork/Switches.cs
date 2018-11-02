e84176e2e4d116d25fa000ccb95a1f86559fd884
Unknown <devyn.brunker@gmail.com>
2018-11-02 15:35:26 -0600

    try doing switches

--- Better Game Test/Assets/Scripts/HomeWork/Switches.cs
+++ Better Game Test/Assets/Scripts/HomeWork/Switches.cs
@@ -4,13 +4,72 @@ using UnityEngine;
 
 public class Switches : MonoBehaviour {
 
-	// Use this for initialization
+	public int num;
+	public string name;
+	public 
+
 	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
+		// switch(num){
+		// 	case 1:
+		// 		print("You picked " + num);
+		// 	break;
+		// 	case 3:
+		// 		print("You picked " + num);
+		// 	break;
+		// 	case 6:
+		// 		print("You picked " + num);
+		// 	break;
+		// 	case 10:
+		// 		print("You picked " + num);
+		// 	break;
+		// 	default:
+		// 		print("I dont understand " + num);
+		// 	break;
+		// }
+
+		// switch(name){
+		// 	case "Jason":
+		// 		print("Welcome " + name + ", we've been waiting for you");
+		// 	break;
+		// 	case "Michael":
+		// 		print("Welcome " + name);
+		// 	break;
+		// 	case "Freddy":
+		// 		print("Welcome to Elm Street " + name);
+		// 	break;
+		// 	default:
+		// 		print("I don't know " + name);
+		// 	break;
+		// }
+
+		switch(){
+			case "":
+				print("");
+			break;
+			case "":
+				print("");
+			break;
+			case "":
+				print("");
+			break;
+			case "":
+				print("");
+			break;
+			default:
+				print("I thought you would be smarter then that");
+			break;
+		}
+
+		// switch(){
+
+		// }
+
+		// switch(){
+
+		// }
+
+		// switch(){
+
+		// }
 	}
 }
