503ad787567fec96c2d9ef4d158adca43ab47bc0
Unknown <devyn.brunker@gmail.com>
2018-11-02 14:11:04 -0600

    Moved some code around and added a bit to loops

---
+++ Better Game Test/Assets/Scripts/HomeWork/Loops.cs
@@ -0,0 +1,65 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Loops : MonoBehaviour {
+
+	//For Loops On/Off
+	public bool A;
+	public bool B;
+	public bool C;
+
+	//While Loops On/Off
+	public bool D;
+	public bool E;
+	public bool F;
+
+	//Variables
+	public int num;
+	private int life = 99;
+
+	// Use this for initialization
+	void Start () {
+
+		//For Loops
+		if(!A){
+			for(int i = 1; i <= 10; i++){
+				print("num = " + i);
+			}
+		}
+		
+		if(!B){
+			for(int l = 99; l >= 0; l--){
+				print("You have " + l + " lifes left");
+				if(l == 0 ){
+					print("You have died");
+				}
+			}
+		}
+
+		if(!C){
+			for(){
+
+			}
+		}
+
+		//While Loops
+		if(!D){
+			while(){
+
+			}
+		}
+
+		if(!E){
+			while(){
+
+			}
+		}
+
+		if(!F){
+			while(){
+
+			}
+		}
+	}
+}
