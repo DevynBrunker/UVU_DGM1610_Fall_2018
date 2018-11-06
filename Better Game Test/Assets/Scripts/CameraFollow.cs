05c3c93e3b310b609a65a3d256115923ae99bcdc
Unknown <devyn.brunker@gmail.com>
2018-11-05 17:08:11 -0700

    trying a camera move

---
+++ Better Game Test/Assets/Scripts/CameraFollow.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFollow : MonoBehaviour {
+
+	public Move PC;
+	public bool isFollowing;
+
+	public float xOffset;
+	public float yOffset;
+
+	// Use this for initialization
+	void Start () {
+		PC = FindObjectOfType<Move>();
+
+		isFollowing = true;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if(isFollowing){
+			transform.postion = new Vector3(PC.transform.postion.x + xOffset, PC.transform.postion.y + yOffset, transform.postion.z);
+		}
+	}
+}
