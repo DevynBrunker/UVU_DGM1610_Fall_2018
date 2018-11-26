e7feb0a29a999010eb729ebadfcdb8625ea81e4b
Unknown <devyn.brunker@gmail.com>
2018-11-26 15:51:15 -0700

    Made Health Manager

---
+++ Better Game Test/Assets/Scripts/HomeWork/HealthManager.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class HealthManager : MonoBehaviour {
+
+	public static int Health;
+	Text HealthText;
+
+	public GameObject Will;
+
+	// Use this for initialization
+	void Start () {
+		HealthText = GetComponent<Text>();
+		Health = 3;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if(Health <= 0){
+			Health = 0;
+		}
+
+		if(Health >= 5){
+			Health = 5;
+		}
+
+		HealthText.text = " " + Health;
+	}
+
+	public static void AddHealth (int HealthToAdd){
+		Health += HealthToAdd;
+	}
+}
