4ffba8a9c29d10af57f6f157ebd86c4f1a883b13
Unknown <devyn.brunker@gmail.com>
2018-11-27 16:37:15 -0700

    added health pickup

--- Better Game Test/Assets/Scripts/HomeWork/HealthManager.cs
+++ Better Game Test/Assets/Scripts/HomeWork/HealthManager.cs
@@ -17,7 +17,7 @@ public class HealthManager : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-		// if(){
+		// if(gameObject.activeSelf){
 		// 	Health = Health - 1;
 		// }
 
---
+++ Better Game Test/Assets/Scripts/HomeWork/HealthPickUp.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HealthPickUp : MonoBehaviour {
+
+	public int HealthToAdd;
+
+	void OnTriggerEnter2D (Collider2D Other){
+		if (Other.GetComponent<Rigidbody2D> () == null)
+			return;
+		
+		HealthManager.AddHealth (HealthToAdd);
+
+		Destroy (gameObject);
+	}
+}
