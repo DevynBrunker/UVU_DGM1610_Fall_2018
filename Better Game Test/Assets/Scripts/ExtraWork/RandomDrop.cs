55fa678d6a010790343eb824a928062d77d29dd5
Unknown <devyn.brunker@gmail.com>
2018-12-03 12:55:14 -0700

    Made random drop but nothing is dropping

--- Better Game Test/Assets/Scripts/ExtraWork/RandomDrop.cs
+++ Better Game Test/Assets/Scripts/ExtraWork/RandomDrop.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 public class RandomDrop : MonoBehaviour {
 
 	//This allows me to make an Aray or List with things inside it
+	[System.Serializable]
 	public class DropTable{
 		public string Name;
 		public GameObject Item;
@@ -12,14 +13,29 @@ public class RandomDrop : MonoBehaviour {
 	}
 
 	//My List
-	public List <DropTable> Table = new List <DropTable>(); 
+	public List <DropTable> LootTable = new List <DropTable>(); 
 	public int DropChance;
 
-	void Loot(){
+	public void Loot(){
+		
+		//This is the drop chance for the enemy
 		int RandomChance = Random.Range (0, 101);
 
 		if(RandomChance <= DropChance){
+			int ItemNumber = 0;
 			
+			for (int i = 0; i < LootTable.Count; i++){
+				ItemNumber += LootTable [i].Chance;
+			}
+
+			int RandomValue = Random.Range (0, ItemNumber);
+			for (int j = 0; j < LootTable.Count; j++){
+				if(RandomValue <= LootTable [j].Chance){
+					Instantiate(LootTable[j].Item, transform.position, Quaternion.identity);
+					return;
+				}
+				RandomValue -= LootTable[j].Chance;
+			}
 		}
 	}
 }
